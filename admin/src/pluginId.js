0458d5713ea4bb8f6e268062bc6e8d74f93e472a
adishare <adie.share@gmail.com>
2024-06-15 09:46:19 +0700

    pluginId

--- admin/src/pluginId.js
+++ admin/src/pluginId.js
@@ -1,5 +1,6 @@
 const pluginPkg = require('../../package.json');
 
-const pluginId = pluginPkg.name.replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
+// const pluginId = pluginPkg.name.replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
+const pluginId = 'import-export-entries';
 
 module.exports = pluginId;
